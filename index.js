5c20165fbe75c4c7ba934a19ed2bb8521b83ebf2
Gabiroto <gabriel.sl@outlook.com>
2017-04-26 01:04:28 -0300

    Adding parameter to url

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ import { Router, Route, hashHistory } from 'react-router';
 import App from './modules/App';
 import About from './modules/About';
 import Repos from './modules/Repos';
+import Repo from './modules/Repo';
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      {/* make them children of `App` */}
+    
+       {/* Repos list page */}
       <Route path="/repos" component={Repos}/>
+
+      {/* Repo specific page */}
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
+
+      {/* About page */}
       <Route path="/about" component={About}/>
     </Route>
   </Router>
